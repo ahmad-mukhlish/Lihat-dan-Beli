feb229d6e309306aab0cf71eaeee3b264c212362
ahmad mukhlis <ahmad_mukhlish_s@yahoo.co.id>
2020-01-06 15:11:47 +0700

    apalah

--- coba.php
+++ coba.php
@@ -1,6 +1,6 @@
 <body>
     <form id="form1" runat="server">
-    <h2>Get Book Information</h2>
+    <h2>Get Book Information cuy</h2>
     Author: <input type="text" id="authorTextbox" value="Patrick Rothfuss" /> <br />
     Title: <input type="text" id="titleTextbox" value="The Name of the Wind" /> <br />
     <input type="button" value="Get Book Information" id="getButton" onclick='getBookInformation()' />
